59d9816187c67261bcf552d7655801f6646e5db3
jhoward14ST <86802470+jhoward14ST@users.noreply.github.com>
2023-12-04 13:06:31 -0800

    Add project files.

---
+++ backend/src/main/java/com/jhoward14ST/YesChef/YesChefApplication.java
@@ -0,0 +1,13 @@
+package com.jhoward14ST.YesChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesChefApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jhoward14ST/YesChef/YesChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhoward14ST.YesChef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
