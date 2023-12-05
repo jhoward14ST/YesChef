84bbaa5d3a5c24e16d4bbf522aa487aad572cec9
jhoward14ST <86802470+jhoward14ST@users.noreply.github.com>
2023-12-04 19:22:14 -0800

    Swap for react native. backend running

--- backend/src/main/java/com/jhoward14ST/YesChef/YesChefApplication.java
+++ backend/src/main/java/com/jhoward14ST/YesChef/YesChefApplication.java
@@ -2,12 +2,19 @@ package com.jhoward14ST.YesChef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class YesChefApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YesChefApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Welcome to the YesChef API";
+	}
 }
