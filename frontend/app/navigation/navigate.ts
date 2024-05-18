0fbb9b1f7a437b443b87524f0ccf83f2bb2ebe16
jhoward14ST <86802470+jhoward14ST@users.noreply.github.com>
2024-05-18 00:45:58 -0700

    Add functions for navigation

--- frontend/app/navigation/navigate.ts
+++ frontend/app/navigation/navigate.ts
@@ -1,5 +1,41 @@
+import throttle from 'lodash/throttle';
+
+const navigateOneTime = (navigate) => throttle(navigate, 1000, { trailing: false });
+
+/* push */
+
+const openRecipes = (navigation) => (props = {}) => {
+    navigation.navigate("Recipes", props);
+};
+
+const openRecipeItem = (navigation) => (props = {}) => {
+    navigation.navigate("RecipeItem", props);
+};
+
+const openInventory = (navigation) => (props = {}) => {
+    navigation.navigate("Inventory", props);
+};
+
+const openInventoryItem = (navigation) => (props = {}) => {
+    navigation.navigate("InventoryItem", props);
+};
+
+const openGroceryList = (navigation) => (props = {}) => {
+    navigation.navigate("GroceryList", props);
+};
+
+const openGroceryListItem = (navigation) => (props = {}) => {
+    navigation.navigate("GroceryListItem", props);
+};
+
 const navigate = (navigation) => ({
-    // TODO: Add the functions for navigation
+    goBack: navigation.goBack,
+    openRecipes: navigateOneTime(openRecipes(navigation)),
+    openRecipeItem: navigateOneTime(openRecipeItem(navigation)),
+    openInventory: navigateOneTime(openInventory(navigation)),
+    openInventoryItem: navigateOneTime(openInventoryItem(navigation)),
+    openGroceryList: navigateOneTime(openGroceryList(navigation)),
+    openGroceryListItem: navigateOneTime(openGroceryListItem(navigation))
 });
 
 export default navigate;
